79794849971bcf2bfce9c6ff514857d9ff4b8678
Tom Neyland <tcneyland+github@gmail.com>
2015-11-25 11:40:55 -0500

    Adds test case for multiple potion blueprints

--- tests/test_api_blueprint.py
+++ tests/test_api_blueprint.py
@@ -6,8 +6,10 @@ from tests import BaseTestCase
 
 
 class BlueprintApiTestCase(BaseTestCase):
+
     def test_api_blueprint(self):
         class SampleResource(ModelResource):
+
             class Meta:
                 name = "samples"
                 model = "samples"
@@ -20,4 +22,37 @@ class BlueprintApiTestCase(BaseTestCase):
         # Register Blueprint
         self.app.register_blueprint(api_bp)
         response = self.client.get("/samples")
+        self.assert200(response)
+
+    def test_multiple_api_blueprints(self):
+        class SampleResource(ModelResource):
+
+            class Meta:
+                name = "samples"
+                model = "samples"
+                manager = MemoryManager
+
+        class SampleResource2(ModelResource):
+
+            class Meta:
+                name = "samples2"
+                model = "samples"
+                manager = MemoryManager
+
+        api_bp = Blueprint("potion_blueprint", __name__.split(".")[0])
+        api = Api(api_bp)
+        api.add_resource(SampleResource)
+
+        api_bp2 = Blueprint("potion_blueprint2", __name__.split(".")[0])
+        api2 = Api(api_bp2)
+        api2.add_resource(SampleResource2)
+
+        # Register Blueprint
+        self.app.register_blueprint(api_bp)
+        self.app.register_blueprint(api_bp2)
+
+        response = self.client.get("/samples")
+        self.assert200(response)
+
+        response = self.client.get("/samples2")
         self.assert200(response)
